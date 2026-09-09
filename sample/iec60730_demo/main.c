464b79d32df3f1416fe23619c65dcb9f53c03592
silabs-HieuV <Hieu.VuTrung@silabs.com>
2026-09-09 16:07:31 +0700

    IOS_DS support build bg21 bg24 demo_cpp

--- sample/iec60730_demo/main.c
+++ sample/iec60730_demo/main.c
@@ -17,36 +17,74 @@
  *
  ******************************************************************************/
 #include "sl_component_catalog.h"
-#include "sl_system_init.h"
-#include "app.h"
-#if defined(SL_CATALOG_POWER_MANAGER_PRESENT)
-#include "sl_power_manager.h"
+
+// Gecko SDK 4.x boots the application through sl_system_*(), while Simplicity
+// SDK 2024.6 and later replaced that service with sl_main_*(). Detect which one
+// the selected SDK provides so this file builds unmodified on both.
+#if defined(SL_CATALOG_SL_MAIN_PRESENT)
+#define IEC60730_DEMO_USE_SL_MAIN
+#elif defined(__has_include)
+#if __has_include("sl_main_init.h")
+#define IEC60730_DEMO_USE_SL_MAIN
 #endif
+#endif
+
+#if defined(IEC60730_DEMO_USE_SL_MAIN)
+#include "sl_main_init.h"
+#if defined(SL_CATALOG_KERNEL_PRESENT)
+#include "sl_main_kernel.h"
+#else // SL_CATALOG_KERNEL_PRESENT
+#include "sl_main_process_action.h"
+#endif // SL_CATALOG_KERNEL_PRESENT
+#else  // IEC60730_DEMO_USE_SL_MAIN
+#include "sl_system_init.h"
 #if defined(SL_CATALOG_KERNEL_PRESENT)
 #include "sl_system_kernel.h"
 #else // SL_CATALOG_KERNEL_PRESENT
 #include "sl_system_process_action.h"
 #endif // SL_CATALOG_KERNEL_PRESENT
+#endif // IEC60730_DEMO_USE_SL_MAIN
+
+#include "app.h"
+
+#if defined(SL_CATALOG_POWER_MANAGER_PRESENT)
+#include "sl_power_manager.h"
+#endif
 
 int main(void)
 {
   // Initialize Silicon Labs device, system, service(s) and protocol stack(s).
   // Note that if the kernel is present, processing task(s) will be created by
   // this call.
+#if defined(IEC60730_DEMO_USE_SL_MAIN)
+  sl_main_init();
+#else
   sl_system_init();
-
-  // Initialize the application. For example, create periodic timer(s) or
-  // task(s) if the kernel is present.
-  app_init();
+#endif
 
 #if defined(SL_CATALOG_KERNEL_PRESENT)
-  // Start the kernel. Task(s) created in app_init() will start running.
+#if defined(IEC60730_DEMO_USE_SL_MAIN)
+  // Start the kernel. The start task completes the component initialization and
+  // calls the app_init() hook before the application tasks run.
+  sl_main_kernel_start();
+#else
+  // Initialize the application, then start the kernel. Task(s) created in
+  // app_init() will start running.
+  app_init();
   sl_system_kernel_start();
+#endif
 #else // SL_CATALOG_KERNEL_PRESENT
+  // Initialize the application. For example, create periodic timer(s).
+  app_init();
+
   while (1) {
     // Do not remove this call: Silicon Labs components process action routine
     // must be called from the super loop.
+#if defined(IEC60730_DEMO_USE_SL_MAIN)
+    sl_main_process_action();
+#else
     sl_system_process_action();
+#endif
 
     // Application process.
     app_process_action();
--- sample/iec60730_demo_cpp/main.cpp
+++ sample/iec60730_demo_cpp/main.cpp
@@ -28,27 +28,65 @@
  *
  ******************************************************************************/
 #include "sl_component_catalog.h"
-#include "sl_main_init.h"
-#if defined(SL_CATALOG_POWER_MANAGER_PRESENT)
-#include "sl_power_manager.h"
+
+// Gecko SDK 4.x boots the application through sl_system_*(), while Simplicity
+// SDK 2024.6 and later replaced that service with sl_main_*(). Detect which one
+// the selected SDK provides so this file builds unmodified on both.
+#if defined(SL_CATALOG_SL_MAIN_PRESENT)
+#define IEC60730_DEMO_USE_SL_MAIN
+#elif defined(__has_include)
+#if __has_include("sl_main_init.h")
+#define IEC60730_DEMO_USE_SL_MAIN
 #endif
+#endif
+
+#if defined(IEC60730_DEMO_USE_SL_MAIN)
+#include "sl_main_init.h"
 #if defined(SL_CATALOG_KERNEL_PRESENT)
 #include "sl_main_kernel.h"
 #else // SL_CATALOG_KERNEL_PRESENT
 #include "sl_main_process_action.h"
 #endif // SL_CATALOG_KERNEL_PRESENT
+#else  // IEC60730_DEMO_USE_SL_MAIN
+#include "sl_system_init.h"
+#if defined(SL_CATALOG_KERNEL_PRESENT)
+#include "sl_system_kernel.h"
+#else // SL_CATALOG_KERNEL_PRESENT
+#include "sl_system_process_action.h"
+#endif // SL_CATALOG_KERNEL_PRESENT
+#endif // IEC60730_DEMO_USE_SL_MAIN
+
+// app_init() and app_process_action() are defined in the C++ translation unit
+// app.cpp; app.h declares them with C linkage so this call site and the
+// definitions agree on the symbol names.
+#include "app.h"
+
+#if defined(SL_CATALOG_POWER_MANAGER_PRESENT)
+#include "sl_power_manager.h"
+#endif
 
 int main(void)
 {
   // Initialize Silicon Labs device, system, service(s) and protocol stack(s).
   // Note that if the kernel is present, the start task will be started and software
   // component initialization will take place there.
+#if defined(IEC60730_DEMO_USE_SL_MAIN)
   sl_main_init();
+#else
+  sl_system_init();
+#endif
 
 #if defined(SL_CATALOG_KERNEL_PRESENT)
+#if defined(IEC60730_DEMO_USE_SL_MAIN)
   // Start the kernel. The start task will be executed (Highest priority) to complete
   // the Simplicity SDK components initialization and the user app_init() hook function will be called.
   sl_main_kernel_start();
+#else
+  // User provided code, then start the kernel. Task(s) created in app_init()
+  // will start running.
+  app_init();
+  sl_system_kernel_start();
+#endif
 #else // SL_CATALOG_KERNEL_PRESENT
 
   // User provided code.
@@ -57,7 +95,11 @@ int main(void)
   while (1) {
     // Silicon Labs components process action routine
     // must be called from the super loop.
+#if defined(IEC60730_DEMO_USE_SL_MAIN)
     sl_main_process_action();
+#else
+    sl_system_process_action();
+#endif
 
     // User provided code. Application process.
     app_process_action();
